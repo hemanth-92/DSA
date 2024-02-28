c9aed8d366fe8d57d44ab10493f6c35caa45469c
hemanth-92 <hemanth9679@gmail.com>
2024-02-28 10:04:12 +0530

    array

--- algorithms/Kadanes.java
+++ algorithms/Kadanes.java
@@ -2,19 +2,10 @@ package algorithms;
 import java.util.Scanner;
 
 public class Kadanes {
-  public static int kadanesAlgorithm(int[] arr) {
-    int currSum = 0;
-    int maxSum = Integer.MIN_VALUE;
-    for(int i =0;i<arr.length;i++){
-      currSum+=arr[i];
-    }
-    maxSum = Math.max(currSum,maxSum);
-    if(currSum<maxSum){
-      currSum =0;
+  public static void main(String[] args){
+    kadanesAlgorithm();
   }
-  return maxSum;
-}
-public static void main(String[] args) {
+  public static void kadanesAlgorithm() {
     Scanner sc = new Scanner(System.in); 
     System.out.print("Enter the size of array : ");
     int n = sc.nextInt();
@@ -24,7 +15,15 @@ public static void main(String[] args) {
       arr[i]=sc.nextInt();
     }
     sc.close();
-    int maxSum=kadanesAlgorithm(arr);
-    System.out.println(maxSum);
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    for(int i =0;i<arr.length;i++){
+      currSum+=arr[i];
+    }
+    maxSum = Math.max(currSum,maxSum);
+    if(currSum<maxSum){
+      currSum =0;
+  }
+  System.out.println(maxSum);;
 }
 }
