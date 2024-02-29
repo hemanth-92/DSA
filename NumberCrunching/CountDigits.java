039c210c360e3c3fa49de973dc097287a3b0658f
hemanth-92 <hemanth9679@gmail.com>
2024-02-29 15:49:12 +0530

    array

---
+++ NumberCrunching/CountDigits.java
@@ -0,0 +1,21 @@
+package NumberCrunching;
+
+import java.util.Scanner;
+
+public class CountDigits {
+  public static void main(String[] args){
+    countDigits();
+  }
+  public static void countDigits() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int n = sc.nextInt();
+    sc.close();
+    int count = 0;
+    while (n > 0) {
+        n = n / 10;
+        count++;
+      }
+      System.out.println(count);
+    }
+}
---
+++ NumberCrunching/HappyNumber.java
@@ -0,0 +1,31 @@
+package NumberCrunching;
+
+public class HappyNumber {
+public static void main(String[] args){
+    happyNumber(7348);
+}
+public static void happyNumber(int n) {
+    int slow = n;
+    int fast = n;
+    do {
+        slow = calculate(slow);
+        fast = calculate(calculate(fast));
+    } while (slow != fast && fast != 1);
+    if (fast == 1) {
+        System.out.println("Yes Happy Number");
+        return;
+    }
+    System.out.println("Not a Happy Number");
+
+}
+
+public static int calculate(int n) {
+    int sum = 0;
+    while (n != 0) {
+        int rem = n % 10;
+        sum = sum + (rem * rem);
+        n = n / 10;
+    }
+    return sum;
+}
+}
