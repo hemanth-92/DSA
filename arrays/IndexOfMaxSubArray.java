af9b7e10ecb37578e27849a245f7e6b53c24c132
hemanth-92 <hemanth9679@gmail.com>
2024-02-29 15:56:34 +0530

    array

--- arrays/IndexOfMaxSubArray.java
+++ arrays/IndexOfMaxSubArray.java
@@ -3,40 +3,40 @@ package arrays;
 import java.util.Scanner;
 
 public class IndexOfMaxSubArray {
-  public static void main(String[] args) {
+public static void main(String[] args) {
     indexOfMaximumSumOfSubarray();
+}
+public static void indexOfMaximumSumOfSubarray() {
+    int maxSum = 0;
+    int currentSum = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    System.out.print("Enter array values: ");
+    for (int i = 0; i < arr.length; i++) {
+          arr[i] = sc.nextInt();
+      }
+    sc.close();
+
+    int startIndex = 0;
+    int endIndex = 0;
+    int tempIndex = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      currentSum += arr[i];
+      if (currentSum < 0) {
+        currentSum = 0;
+        tempIndex = i;
+      }
+      if (currentSum > maxSum) {
+      maxSum = currentSum;
+      startIndex = tempIndex;
+      endIndex = i;
+    }
   }
-  public static void indexOfMaximumSumOfSubarray() {
-        int maxSum = 0;
-        int currentSum = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.print("Enter array values: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-        sc.close();
-
-        int startIndex = 0;
-        int endIndex = 0;
-        int tempIndex = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            currentSum += arr[i];
-            if (currentSum < 0) {
-                currentSum = 0;
-                tempIndex = i;
-            }
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-                startIndex = tempIndex;
-                endIndex = i;
-            }
-        }
-        System.out.println("Start index: " + startIndex);
-        System.out.println("End index: " + endIndex);
-        System.out.println(maxSum);
+    System.out.println("Start index: " + startIndex);
+    System.out.println("End index: " + endIndex);
+    System.out.println(maxSum);
     }
 }
